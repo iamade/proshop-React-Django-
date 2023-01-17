4de751cc242f44198d1bcaad9b22e600bb64e8c8
Adesegun Koiki <adesegun.koiki@sterling.ng>
2023-01-17 19:12:11 +0100

    homescreen_started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <div>
       <Header/>
-      <main>
-        <h1>Welcome</h1>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+       
       </main>
       <Footer/>
     </div>
