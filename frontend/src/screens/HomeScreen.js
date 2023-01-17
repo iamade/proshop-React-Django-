e5be7eef324ceb5a1d0fd9c763c215289abcf3db
Adesegun Koiki <adesegun.koiki@sterling.ng>
2023-01-17 19:36:25 +0100

    card done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import products from '../products'
 
-import products from '../components/Product'
+import Product from '../components/Product'
 
 function HomeScreen() {
   return (
