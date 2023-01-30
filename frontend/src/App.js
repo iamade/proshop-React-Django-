6cdf3edfb0146344830f07cdf78b4faf624fed24
Adesegun Koiki <kamar3deen@gmail.com>
2023-01-30 18:26:50 +0100

    qty select & Add to cart done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { Container } from 'react-bootstrap'
-import {BrowserRouter, Routes, Router, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           
           <Route path='/' element={<HomeScreen />} exact />
           <Route path='/product/:id' element={<ProductScreen />} />
+          <Route path='/cart/:id?' element={<CartScreen />} />
           </Routes>
           
         </Container>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,96 +1,140 @@
-import React, {useState, useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import Rating from '../components/Rating' 
-import Loader from '../components/Loader' 
-import Message from '../components/Message' 
-import { useDispatch, useSelector } from 'react-redux'  
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useDispatch, useSelector } from "react-redux";
 
 // import products from '../products'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails } from "../actions/productActions";
 // import axios from 'axios'
 
+
+
 function ProductScreen() {
-    const match = useParams() 
+  const match = useParams();
+  const [qty, setQty] = useState(1);
+  let navigate = useNavigate();
 
-    const dispatch = useDispatch()
-    const productDetails = useSelector(state => state.productDetails )
-    const {loading, error, product} = productDetails
-    // const product = products.find((p) => p._id == match.id)
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+  // const product = products.find((p) => p._id == match.id)
 
-    // const [product, setProduct] = useState([])
+  // const [product, setProduct] = useState([])
 
-    useEffect(() => {
-        dispatch(listProductDetails(match.id))
-      }, [dispatch, match])
+  useEffect(() => {
+    dispatch(listProductDetails(match.id));
+  }, [dispatch, match]);
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${match.id}?qty=${qty}`)
+  }
 
   return (
     <div>
-        <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-        {loading ?
-            <Loader/>
-            : error
-                ? <Message variant='danger'>{error}</Message>    
-            :( 
-                <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-    
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
-                        </ListGroup.Item>
-    
-                        <ListGroup.Item>
-                            Price: {product.price}
-                        </ListGroup.Item>
-    
-                        <ListGroup.Item>
-                            Description: {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-    
-                    
-                </Col>
-    
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Price:</Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-    
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Status:</Col>
-                                    <Col>
-                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-    
-                            <ListGroup.Item>
-                                <Button className='w-100' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
-            )
-    }
-       
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                  color={"#f8e825"}
+                />
+              </ListGroup.Item>
+
+              <ListGroup.Item>Price: {product.price}</ListGroup.Item>
+
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
+                <ListGroup.Item>
+                  <Button
+                    onClick={addToCartHandler}
+                    className="w-100"
+                    disabled={product.countInStock === 0}
+                    type="button"
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
